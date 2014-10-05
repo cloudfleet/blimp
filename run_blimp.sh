87102d2d0d2c7d8dfb30286f20615ae0df5cdc01
DoubleMalt <christoph@web.crofting.com>
2014-10-05 13:41:54 +0200

    added hostname and split command to make it more readable

--- run_blimp.sh
+++ run_blimp.sh
@@ -1,4 +1,10 @@
 #! /bin/bash
 
-sudo docker run --privileged -it -e "CLOUDFLEET_DOMAIN=${1}.test.blimpyard.cloudfleet.io" -e "CLOUDFLEET_SECRET=password" -e "CLOUDFLEET_HOST=blimpyard.cloudfleet.io:443" --name "blimp_${1}" cloudfleet/blimp 
+sudo docker run --privileged -it \
+  -e "CLOUDFLEET_DOMAIN=${1}.test.blimpyard.cloudfleet.io" \
+  -e "CLOUDFLEET_SECRET=password" \
+  -e "CLOUDFLEET_HOST=blimpyard.cloudfleet.io:443" \
+  --name "blimp_${1}" \
+  --hostname "blimp" \
+  cloudfleet/blimp 
 
