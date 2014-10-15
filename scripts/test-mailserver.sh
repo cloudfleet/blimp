ced27ca6e5be0b77b39346e64cf780974c6b869a
DoubleMalt <christoph@web.crofting.com>
2014-10-15 23:11:57 +0200

    add mailserver testscript

---
+++ scripts/test-mailserver.sh
@@ -0,0 +1,1 @@
+wget http://www.jetmore.org/john/code/swaks/files/swaks-20130209.0/swaks && chmod +x ./swaks && ./swaks -f xyz@doublemalt.bonniecloud.com -t christoph.witzany@gmail.com -s blimp-docker -p 25
