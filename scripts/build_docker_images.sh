8b5356fc5933e506d823152444352c46dc313a9f
Dražen Lučanin <kermit666@gmail.com>
2014-12-20 12:43:46 +0000

    ARM Debian image and build Mailpile

--- scripts/build_docker_images.sh
+++ scripts/build_docker_images.sh
@@ -3,20 +3,35 @@
 # run as:
 # ./scripts/build_docker_images.sh scripts/docker_images.txt
 
+#TODO: https://github.com/docker-library/node.git /0.10/slim
+
 docker_images_file=$1
 base_repo_url=https://github.com/cloudfleet
 build_location=/tmp/docker_images
 
+function fetch_code(){
+    repo_url=$1
+    repo_dir=$2
+    if [ -d $repo_dir ]; then
+        # if true this block of code will execute
+        echo "folder exists, not cloning" # pull changes?
+    else
+        echo "cloning https://github.com/cloudfleet/$line"
+        git clone --depth=1 $repo_url $repo_dir
+    fi
+}
+
 function build_image(){
     image=$1
     echo building $image
-    git clone --depth=1 \
-        $base_repo_url/$image \
-        $build_location/$line
-        cd /tmp/docker_images/$line
-        # sed -i 's/ubuntu:14.04/hominidae\/armhf-ubuntu/g' Dockerfile
-        # sed -i 's/debian/armbuild\/debian/g' Dockerfile
-        sudo docker build -t $line .
+    repo_dir=$build_location/$line
+    fetch_code $base_repo_url/$image $repo_dir
+    cd $repo_dir
+    sed -i 's/ubuntu:14.04/hominidae\/armhf-ubuntu/g' Dockerfile
+    sed -i 's/debian/mazzolino\/armhf-debian/g' Dockerfile
+    image_name=`echo "cloudfleet/$image" | sed 's/./\L&/g'` # lowercase
+    echo "building Docker image as $image_name"
+    sudo docker build -t $image_name .
 }
 
 function clean_up(){
