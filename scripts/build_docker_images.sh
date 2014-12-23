7bea768a5068cdfdcf76c6ef336c0be19ac915d0
Dražen Lučanin <kermit666@gmail.com>
2014-12-23 17:00:56 +0100

    more general docker_images.txt

--- scripts/build_docker_images.sh
+++ scripts/build_docker_images.sh
@@ -8,7 +8,6 @@
 #TODO: https://github.com/docker-library/node.git /0.10/slim
 
 docker_images_file=$1
-base_repo_url=https://github.com/cloudfleet
 build_location=${2:-/tmp/docker_images}
 
 function fetch_code(){
@@ -23,15 +22,21 @@ function fetch_code(){
     fi
 }
 
-function build_image(){
-    image=$1
-    repo_dir=$build_location/$line
-    echo "fetching $image to $repo_dir"
-    fetch_code $base_repo_url/$image $repo_dir
-    cd $repo_dir
+function patch_dockerfile(){
     sed -i 's/ubuntu:14.04/hominidae\/armhf-ubuntu/g' Dockerfile
     sed -i 's/debian/mazzolino\/armhf-debian/g' Dockerfile
-    image_name=`echo "cloudfleet/$image" | sed 's/./\L&/g'` # lowercase
+    sed -i 's/node:slim/node-armhf/g' Dockerfile
+}
+
+function build_image(){
+    repo_url=$1
+    image_name=$2
+    dockerfile_path=$3
+    repo_dir=$build_location/$image_name
+    echo "fetching $repo_url to $repo_dir"
+    fetch_code $repo_url $repo_dir
+    cd $repo_dir/$dockerfile_path
+    image_name=`echo "$image_name" | sed 's/./\L&/g'` # lowercase
     echo "building Docker image as $image_name"
     sudo docker build -t $image_name .
 }
@@ -41,7 +46,7 @@ function clean_up(){
 }
 
 function build_all_images(){
-    echo "building images from $base_repo_url in $build_location"
+    echo "building images from $docker_images_file in $build_location"
     while read line; do
         build_image $line
     done < $docker_images_file
