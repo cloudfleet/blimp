c6fd32bcc8fcf416938a3ab7de24686d213c6d00
Dražen Lučanin <kermit666@gmail.com>
2014-12-27 14:09:04 +0100

    tag docker pulls in ansible scripts

--- scripts/start-docker.sh
+++ scripts/start-docker.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 scripts/wrapdocker.sh
+# TODO for ARM:
+# scripts/build_docker_images.sh scripts/docker_images.txt /root/docker_images
+# ansible-playbook blimp-start.yml --skip-tags=docker-pull
 (cd scripts/ansible && ansible-playbook blimp-start.yml)
 /bin/bash
-
