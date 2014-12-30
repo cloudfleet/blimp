980bb772bd8bcbefc0067a2ad50a3d7231fc3ac5
Dražen Lučanin <kermit666@gmail.com>
2014-12-30 13:59:21 +0100

    start-docker.sh: on ARM build images, don't pull

--- scripts/start-docker.sh
+++ scripts/start-docker.sh
@@ -1,7 +1,28 @@
 #!/bin/bash
+
+start_arm(){
+    echo "start on ARM"
+    scripts/build_docker_images.sh scripts/docker_images.txt /root/docker_images
+    cd scripts/ansible
+    ansible-playbook blimp-start.yml --skip-tags=docker-pull
+}
+
+start_not_arm(){
+    echo "start on non-ARM"
+    (cd scripts/ansible && ansible-playbook blimp-start.yml)
+}
+
 scripts/wrapdocker.sh
-# TODO for ARM:
-# scripts/build_docker_images.sh scripts/docker_images.txt /root/docker_images
-# ansible-playbook blimp-start.yml --skip-tags=docker-pull
-(cd scripts/ansible && ansible-playbook blimp-start.yml)
+
+# check for ARM
+if hash dpkg 2>/dev/null; then # if dpkg available
+    if [ "`dpkg --print-installation-architecture`" = "arm" ]; then
+        start_arm
+    else
+        start_not_arm
+    fi
+else
+    start_not_arm
+fi
+
 /bin/bash
