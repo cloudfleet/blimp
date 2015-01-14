de65c9c36206a40217af82e36d84c07aa4e5260b
Dražen Lučanin <kermit666@gmail.com>
2015-01-14 22:38:44 +0100

    install.sh adapted for ARM

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,5 +4,24 @@ sudo apt-get update
 sudo apt-get install -y ansible python-apt python-pycurl apt-transport-https python-pip iptables ca-certificates lxc git
 sudo pip install ansible --upgrade
 
-(cd scripts/ansible && ansible-playbook blimp-install.yml)
+install_arm(){
+    echo "install on ARM"
+    cd scripts/ansible
+    ansible-playbook blimp-install.yml --skip-tags=skip-physical-blimp
+}
 
+install_not_arm(){
+    echo "install on non-ARM"
+    (cd scripts/ansible && ansible-playbook blimp-install.yml)
+}
+
+# check for ARM
+if hash dpkg 2>/dev/null; then # if dpkg available
+    if [ "`dpkg --print-architecture`" = "armhf" ]; then
+        install_arm
+    else
+        install_not_arm
+    fi
+else
+    install_not_arm
+fi
