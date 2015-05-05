3aa80a2ad28df1afa6424bdd5ef12d3971dd3f05
Christoph Witzany <christoph@web.crofting.com>
2015-05-05 17:02:22 +0200

    add missing cd

--- scripts/autoupdate/update-and-restart.sh
+++ scripts/autoupdate/update-and-restart.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-(`dirname $0` && ./update-images.sh && ./stop-containers.sh && ./start-containers.sh)
+(cd `dirname $0` && ./update-images.sh && ./stop-containers.sh && ./start-containers.sh)
