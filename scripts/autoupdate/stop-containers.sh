a5d3436c5d26c0425620ee19564ad9a43cfd1aff
Christoph Witzany <christoph@web.crofting.com>
2015-05-31 23:59:53 +0200

    remove untagged containers

--- scripts/autoupdate/stop-containers.sh
+++ scripts/autoupdate/stop-containers.sh
@@ -4,3 +4,8 @@ if [ -n "$CONTAINERS_TO_DELETE" ]; then
   docker stop $CONTAINERS_TO_DELETE
   docker rm $CONTAINERS_TO_DELETE
 fi
+
+IMAGES_TO_REMOVE=$(docker images | grep "^<none>" | awk '{print $3}')
+if [ -n "$IMAGES_TO_DELETE" ]; then
+  docker rmi -f $IMAGES_TO_DELETE
+fi
