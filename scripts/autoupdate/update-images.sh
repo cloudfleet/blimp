eb0c3833a706ff94ee35d2f6544e4d4b7f78dbfa
Christoph Witzany <christoph@web.crofting.com>
2015-06-09 16:38:29 +0200

    add pagekite to pull

--- scripts/autoupdate/update-images.sh
+++ scripts/autoupdate/update-images.sh
@@ -3,11 +3,10 @@ echo "=================================="
 echo " Fetching new image versions ..."
 echo "=================================="
 DOCKER_REGISTRY=registry.marina.io
-APPS="cockpit mailbox musterroll mailpile wellknown radicale nginx conduit"
+APPS="cockpit mailbox musterroll mailpile wellknown radicale nginx conduit pagekite"
 for app in $APPS; do
   docker pull $DOCKER_REGISTRY/cloudfleet/blimp-$app | grep Status
 done
 echo "=================================="
 echo " Fetched new image versions."
 echo "=================================="
-
