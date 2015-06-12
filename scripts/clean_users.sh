e296f4108bc56330cd19a04a686e2c9bd1018bee
Christoph Witzany <christoph@web.crofting.com>
2015-06-12 16:35:07 +0200

    dockerize doveshed and streamline directory structure

--- scripts/clean_users.sh
+++ scripts/clean_users.sh
@@ -1,6 +1,4 @@
-supervisorctl restart conduit
 docker rm -f `docker ps -aq`
-docker rm -f musterroll
 rm /opt/cloudfleet/common/users.json
 rm /opt/cloudfleet/conf/port-assignments.json
 (cd scripts/ansible && ansible-playbook blimp-start-containers.yml --skip-tags=skip-physical-blimp)
--- scripts/test-mailserver.sh
+++
@@ -1,1 +0,0 @@
-wget http://www.jetmore.org/john/code/swaks/files/swaks-20130209.0/swaks && chmod +x ./swaks && ./swaks -f xyz@doublemalt.bonniecloud.com -t christoph.witzany@gmail.com -s blimp-docker -p 25
