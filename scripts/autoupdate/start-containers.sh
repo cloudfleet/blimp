67cfee347b10564f6f9566077d0f8f7c67cf4ad9
Christoph Witzany <christoph@web.crofting.com>
2015-06-15 11:41:38 +0200

    big cleanup

--- scripts/autoupdate/start-containers.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "=============================="
-echo "  Starting containers ... "
-echo "=============================="
-(cd ../ansible && ansible-playbook blimp-start-containers.yml --skip-tags=skip-physical-blimp | grep -A 3 "PLAY RECAP")
-echo "=============================="
-echo "  Started containers. "
-echo "=============================="
