63b16d3f7ee98e73e7370d2eed716177fb1db55b
Christoph Witzany <christoph@web.crofting.com>
2015-06-16 08:36:54 +0200

    revive vagrant installation and adjust for engineroom

--- scripts/install-vagrant.sh
+++ scripts/install-vagrant.sh
@@ -1,6 +1,28 @@
 #! /bin/bash
 export CLOUDFLEET_DOMAIN=$1
 export CLOUDFLEET_SECRET=$2
-export CLOUDFLEET_HOST=$3
-set
-(cd /vagrant && scripts/install.sh && scripts/start.sh)
+
+apt-get -y update && apt-get install python-pip wget
+wget -qO- https://get.docker.com/ | sh
+
+pip install meta-compose
+
+echo <<EOF
+
+CLOUDFLEET_DOMAIN=$CLOUDFLEET_DOMAIN
+CLOUDFLEET_SECRET=$CLOUDFLEET_SECRET
+CLOUDFLEET_REGISTRY=docker.io
+
+EOF > /tmp/blimp-vars.sh
+
+UPDATE_CRONTAB="0 4 * * * /opt/cloudfleet/engineroom/upgrade-blimp.sh >> /opt/cloudfleet/data/logs/upgrade.log 2>&1"
+
+{ crontab -l -u root; echo '$UPDATE_CRONTAB'; } | crontab -u root -
+
+mkdir -p /opt/cloudfleet
+cd /opt/cloudfleet
+git clone https://github.com/cloudfleet/blimp-engineroom.git engineroom
+
+bin/initialize-blimp.sh
+
+bin/upgrade-blimp.sh
