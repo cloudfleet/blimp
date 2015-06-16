143a19f3ee6e09728c3935b988136117f06ebde4
Christoph Witzany <christoph@web.crofting.com>
2015-06-16 13:27:56 +0200

    fix install script for ubuntu

--- scripts/install-vagrant.sh
+++ scripts/install-vagrant.sh
@@ -2,26 +2,37 @@
 export CLOUDFLEET_DOMAIN=$1
 export CLOUDFLEET_SECRET=$2
 
-apt-get -y update && apt-get install python-pip wget
-wget -qO- https://get.docker.com/ | sh
+echo "deb https://get.docker.io/ubuntu docker main" > /etc/apt/sources.list.d/docker.list
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
 
-pip install meta-compose
+apt-get -y update && apt-get -y install python-pip lxc-docker
 
-echo <<EOF
+
+pip install meta-compose requests==2.5.2
+
+cat > /tmp/blimp-vars.sh <<EOF
 
 CLOUDFLEET_DOMAIN=$CLOUDFLEET_DOMAIN
 CLOUDFLEET_SECRET=$CLOUDFLEET_SECRET
-CLOUDFLEET_REGISTRY=docker.io
+CLOUDFLEET_REGISTRY=registry.hub.docker.com
 
-EOF > /tmp/blimp-vars.sh
+EOF
 
-UPDATE_CRONTAB="0 4 * * * /opt/cloudfleet/engineroom/upgrade-blimp.sh >> /opt/cloudfleet/data/logs/upgrade.log 2>&1"
+UPDATE_CRONTAB='0 4 * * * /opt/cloudfleet/engineroom/bin/upgrade-blimp.sh >> /opt/cloudfleet/data/logs/upgrade.log 2>&1'
 
-{ crontab -l -u root; echo '$UPDATE_CRONTAB'; } | crontab -u root -
+echo $UPDATE_CRONTAB
+echo "$UPDATE_CRONTAB" | crontab -u root -
 
-mkdir -p /opt/cloudfleet
+mkdir -p /opt/cloudfleet/data/logs
 cd /opt/cloudfleet
-git clone https://github.com/cloudfleet/blimp-engineroom.git engineroom
+
+if [ ! -d /opt/cloudfleet/engineroom ]; then
+  git clone https://github.com/cloudfleet/blimp-engineroom.git engineroom
+else
+  (cd engineroom && git pull)
+fi
+
+cd engineroom
 
 bin/initialize-blimp.sh
 
